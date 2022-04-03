9dec415843a1ca7ae61c0b33d592a178cec261d4
Emerson Bezerra <emersondevelops@gmail.com>
2022-04-03 12:23:32 -0300

    Initial commit

---
+++ src/main/kotlin/dev/vidaemcorrotinaapi/VidaEmCorrotinaApplication.kt
@@ -0,0 +1,11 @@
+package dev.vidaemcorrotinaapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class VidaEmCorrotinaApplication
+
+fun main(args: Array<String>) {
+	runApplication<VidaEmCorrotinaApplication>(*args)
+}
